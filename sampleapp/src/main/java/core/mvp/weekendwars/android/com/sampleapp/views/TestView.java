b41092ee8722402acf60dbc09e34151aa804fcfe
Ezequiel <ezebongiovi@gmail.com>
2018-06-19 13:24:38 -0300

    Initial Setup

---
+++ sampleapp/src/main/java/core/mvp/weekendwars/android/com/sampleapp/views/TestView.java
@@ -0,0 +1,7 @@
+package core.mvp.weekendwars.android.com.sampleapp.views;
+
+import core.mvp.weekendwars.android.com.views.AbstractView;
+
+public interface TestView extends AbstractView {
+    void displayRestoredState(long savedState);
+}
