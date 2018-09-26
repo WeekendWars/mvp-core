e88b3fdfc8b62d2f93150d8040476ef809c81a23
Ezequiel <ezebongiovi@gmail.com>
2018-09-26 11:34:11 -0300

    Removed unnecessary test. Robolectric required

--- core/src/test/java/com/weekendwars/core/mvp/presenters/MVPUnitTest.java
+++ core/src/test/java/com/weekendwars/core/mvp/presenters/MVPUnitTest.java
@@ -19,36 +19,12 @@ import static org.junit.Assert.assertTrue;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class MVPUnitTest {
-    @SuppressWarnings({"unchecked", "ConstantConditions", "CPD-START"})
+
+    // TODO: Use Robolectric for testing activity's lifecycle
+
+    @SuppressWarnings({"unchecked", "ConstantConditions"})
     @Test
     public void testActivityLifeCycle() {
-        final AbstractPresenter presenter = Mockito.spy(AbstractPresenter.class);
-        assertNull("Disposable should be null", presenter.getDisposable());
-
-        presenter.attachView(Mockito.spy(AbstractActivity.class));
-        assertNotNull("Disposable shouldn't be null", presenter.getDisposable());
-
-        presenter.addDisposable(new ConsumerSingleObserver<>(new Consumer<Object>() {
-            @Override
-            public void accept(final Object o) {
-                // Nothing to do
-            }
-        }, new Consumer<Throwable>() {
-            @Override
-            public void accept(final Throwable throwable) {
-                // Nothing to do
-            }
-        }));
-
-        assertTrue("Presenter should have subscription", presenter.hasSubscriptions());
-
-        presenter.detachView();
-        assertFalse("Presenter should not have subscription", presenter.hasSubscriptions());
-    }
-
-    @SuppressWarnings({"unchecked", "ConstantConditions", "CPD-END"})
-    @Test
-    public void testFragmentLifeCycle() {
         final AbstractPresenter presenter = Mockito.spy(AbstractPresenter.class);
         assertNull("Disposable should be null", presenter.getDisposable());
 
